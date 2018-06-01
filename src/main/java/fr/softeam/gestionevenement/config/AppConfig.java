03f2dc0192dbf017dcde706a2a1d6a0cbda61912
pjb <pjb>
2018-06-01 15:02:47 +0200

    Correction import

--- src/main/java/fr/softeam/gestionevenement/config/AppConfig.java
+++ src/main/java/fr/softeam/gestionevenement/config/AppConfig.java
@@ -1,6 +1,5 @@
 package fr.softeam.gestionevenement.config;
 
-import org.postgresql.jdbc2.optional.SimpleDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
